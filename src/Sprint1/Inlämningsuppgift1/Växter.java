768f3b8fc5c17fe81e37613db576841bdafae151
Ninib Masso <ninibmasso@hotmail.com>
2023-10-01 14:19:08 +0200

    Added comment

--- src/Sprint1/Inlämningsuppgift1/Växter.java
+++ src/Sprint1/Inlämningsuppgift1/Växter.java
@@ -3,6 +3,7 @@ package Sprint1.Inlämningsuppgift1;
 public class Växter implements LiquidPerDay{
 
     private String namn; //inkapsulering
+    //mbf
     private double längd; //inkapsulering
     protected String sortsVätska; //inkapsulering
     protected double mängdVätska; //inkapsulering
